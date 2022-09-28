66d09f4ef61364d0847cad9f53fbcc0d06b5ba47
HuyTa <huytq270397@gmail.com>
2022-09-28 11:15:20 +0700

    + Init source

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+import TQHButton from '@/components/atoms/TQHButton.vue';
+</script>
+<template>
+  <TQHButton>test</TQHButton>
+</template>
---
+++ src/components/atoms/TQHButton.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { PropType } from 'vue';
+
+type ButtonTypes = 'button' | 'submit' | 'reset' | undefined;
+
+defineProps({
+  type: {
+    type: String as PropType<ButtonTypes>,
+    required: false,
+    default: 'button',
+    validator: (value: string) => {
+      const buttonTypes = ['button', 'submit', 'reset'];
+      return buttonTypes.includes(value);
+    },
+  },
+});
+</script>
+
+<template>
+  <button class="tqh-button" :type="type">
+    <slot />
+  </button>
+</template>
